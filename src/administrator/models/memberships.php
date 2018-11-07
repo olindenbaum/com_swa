8de9fc409dea8b8f3edf81bb6b3f29bee0b33e52
Oliver Shotton <geekman92@gmail.com>
2018-11-07 00:25:31 +0000

    WIP - started on memberships admin table

---
+++ src/administrator/models/memberships.php
@@ -0,0 +1,117 @@
+<?php
+
+defined('_JEXEC') or die;
+
+jimport('joomla.application.component.modellist');
+
+class SwaModelMemberships extends JModelList
+{
+	/**
+	 * SwaModelMemberships constructor.
+	 *
+	 * @param array $config An optional associative array of configuration settings.
+	 *
+	 * @see   JModelList
+	 *
+	 * @since 0.2
+	 */
+	public function __construct(array $config = array())
+	{
+		if (empty($config['filter_fields']))
+		{
+			$config['filter_fields'] = array(
+				'id',
+				'member_id',
+				'university_id',
+			);
+		}
+
+		parent::__construct($config);
+	}
+
+	protected function populateState($ordering = null, $direction = null)
+	{
+		$app    = JFactory::getApplication('administrator');
+		$search = $app->getUserStateFromRequest($this->context . '.filter.search', 'filter_search');
+		$this->setState('filter.search', $search);
+		$this->setState('params', JComponentHelper::getParams('com_swa'));
+		parent::populateState('id', 'desc');
+	}
+
+	/**
+	 * Method to get a store id based on model configuration state.
+	 *
+	 * This is necessary because the model is used by the component and
+	 * different modules that might need different sets of data or different
+	 * ordering requirements.
+	 *
+	 * @param   string $id A prefix for the store id.
+	 *
+	 * @return    string        A store id.
+	 */
+	protected function getStoreId($id = '')
+	{
+		// Compile the store id.
+		$id .= ':' . $this->getState('filter.search');
+
+		return parent::getStoreId($id);
+	}
+
+	/**
+	 * Build an SQL query to load the list data.
+	 *
+	 * @return    JDatabaseQuery
+	 */
+	protected function getListQuery()
+	{
+		// Create a new query object.
+		$db    = $this->getDbo();
+		$query = $db->getQuery(true);
+
+		// Select the required fields from the table.
+		$query->select($this->getState('list.select', 'DISTINCT membership.*'));
+		$query->select('user.name AS member');
+		$query->select('uni.name');
+
+		$query->from('#__swa_membership AS membership');
+		$query->leftJoin('#__swa_member AS member ON member.id = membership.member_id');
+		$query->leftJoin('#__users AS user ON user.id = member.user_id');
+		$query->leftJoin('#__swa_university AS uni ON uni.id = membership.uni_id');
+
+		// Filter by search in title
+		$search = $this->getState('filter.search');
+		if (!empty($search))
+		{
+			if (stripos($search, 'id:') === 0)
+			{
+				$query->where('member.id = ' . (int) substr($search, 3));
+			}
+			else
+			{
+				$search = $db->Quote('%' . $db->escape($search, true) . '%');
+				$query->where(
+					'( member LIKE ' . $search .
+					'  OR  user.username LIKE ' . $search . ' )'
+				);
+			}
+		}
+
+		// Add the list ordering clause.
+		$orderCol  = $this->state->get('list.ordering');
+		$orderDirn = $this->state->get('list.direction');
+		if ($orderCol && $orderDirn)
+		{
+			$query->order($db->escape($orderCol . ' ' . $orderDirn));
+		}
+
+		return $query;
+	}
+
+	public function getItems()
+	{
+		$items = parent::getItems();
+
+		return $items;
+	}
+
+}
